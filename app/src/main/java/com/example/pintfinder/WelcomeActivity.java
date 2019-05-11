032d1822bb77243828f730715d0e359252344ba6
Alessandro <alessandro_lopresti@hotmail.it>
2019-05-11 10:53:20 +0200

    login-beta

---
+++ app/src/main/java/com/example/pintfinder/WelcomeActivity.java
@@ -0,0 +1,40 @@
+package com.example.pintfinder;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class WelcomeActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_welcome);
+
+        // LOGIN BUTTON
+
+        Button login = findViewById(R.id.login);
+        findViewById(R.id.login).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(WelcomeActivity.this, LoginActivity.class);
+                startActivity(intent);
+                }
+        });
+
+        // SIGN-UP BUTTON
+
+        Button signup = findViewById(R.id.signupButton);
+        findViewById(R.id.signupButton).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(WelcomeActivity.this, SignUpActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+
+}
