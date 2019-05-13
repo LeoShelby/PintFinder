0b979c76e958ff21b4bd815904e5984dd506c03e
Alessandro <alessandro_lopresti@hotmail.it>
2019-05-13 15:15:41 +0200

    colors in general

--- app/src/main/java/com/example/pintfinder/LoginActivity.java
+++ app/src/main/java/com/example/pintfinder/LoginActivity.java
@@ -26,7 +26,7 @@ public class LoginActivity extends AppCompatActivity {
                 String username = usernameView.getText().toString();
                 if (username.equals("angelo")) {
                     // PubLover
-                    Intent intent = new Intent(LoginActivity.this, ListBeersActivity.class);//HomePageLover.class);
+                    Intent intent = new Intent(LoginActivity.this, HomePageLover.class); //ListBeersActivity.class
                     intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK); // con questo comando vengono eliminate dalla stack le activity di login in modo che se viene premuto il back button dalla home si esce direttamente dall'app, solo con il bottone di LOG OUT si può tornare all'activity di LOGIN!
                     startActivity(intent);
                 }
