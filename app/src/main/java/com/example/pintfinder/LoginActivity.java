5e442cc5624c8b9dfcefdc0fa510ed1eac048433
LeoShelby <leo-dp-95@hotmail.it>
2019-05-16 17:12:01 +0200

    stack deletion deleted

--- app/src/main/java/com/example/pintfinder/LoginActivity.java
+++ app/src/main/java/com/example/pintfinder/LoginActivity.java
@@ -10,8 +10,6 @@ import android.widget.EditText;
 import android.widget.Toast;
 
 public class LoginActivity extends AppCompatActivity {
-    ProgressDialog progressDialog;
-
 
     @Override
 
@@ -27,24 +25,18 @@ public class LoginActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 String username = usernameView.getText().toString();
-                progressDialog = new ProgressDialog(LoginActivity.this);
-                progressDialog.setMessage("Loading..."); // Setting Message
-                progressDialog.setTitle("Login"); // Setting Title
-                progressDialog.setProgressStyle(ProgressDialog.STYLE_SPINNER); // Progress Dialog Style Spinner
-                progressDialog.show(); // Display Progress Dialog
-                progressDialog.setCancelable(false);
 
 
                 if (username.equals("georg")) {
                     // PubLover
                     Intent intent = new Intent(LoginActivity.this, HomePageLover.class);//ListBeersActivity.class);//HomePageLover.class);
-                    intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK); // con questo comando vengono eliminate dalla stack le activity di login in modo che se viene premuto il back button dalla home si esce direttamente dall'app, solo con il bottone di LOG OUT si può tornare all'activity di LOGIN!
+                    //intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK); // con questo comando vengono eliminate dalla stack le activity di login in modo che se viene premuto il back button dalla home si esce direttamente dall'app, solo con il bottone di LOG OUT si può tornare all'activity di LOGIN!
                     startActivity(intent);
                 }
                 else if (username.equals("maria"))  {
                     // Pub Owner
                     Intent intent = new Intent(LoginActivity.this, HomePageOwner.class); // sostituire MainActivity.class con il nome dell'Activity riguardante l'homepage del PubOwner
-                    intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                    //intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
                     startActivity(intent);
                 }
                 else {
